ff24e64e27b417c68ceabd4bb738f9ffb5e943cc
Jackyso2000 <46666550+Jackyso2000@users.noreply.github.com>
2025-10-14 19:13:23 +0800

    Revert "X"
    This reverts commit bd6906eaa1d0876af6ffc928b7c0ac4a152088e3.

---
+++ web/types/product.ts
@@ -0,0 +1,10 @@
+import type { Image as SanityImage } from 'sanity'
+
+export interface Product {
+  _id: string
+  name: string
+  slug: { current: string }
+  price: number
+  mainImage: SanityImage
+}
+
